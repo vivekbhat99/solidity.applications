6c9b6b9365d8fbc16cd4fca9071db80ba2e122be
vivekbhat99 <vbhat99999@gmail.com>
2020-08-18 12:58:23 +0530

    phase1-sc-deployedlocally

--- todo/contracts/todolist.sol
+++ todo/contracts/todolist.sol
@@ -1,5 +1,26 @@
 pragma solidity 0.5.16;
 
 contract todolist{
-uint16 public tasks =0;
+uint16 public tasks = 0; //count the tasks
+
+struct Todo{
+    uint16 id;
+    string taskToPerform;
+    bool isCompleted;
+}
+
+mapping (uint16 => Todo) public allTasks;  //map task number to Todo struct
+
+constructor() public{
+    listTask("Check your mails"); //default task to load while deployed
+}
+
+
+function listTask(string memory _task) public {
+    tasks++;
+    allTasks[tasks] = Todo(tasks, _task, false);
+}
+
+
+
 }
